73750f5d4fed0af83d956e42bdd4b1b8a07cb9e9
who-Is-Beto <roberto.cortes13233@gmail.com>
2021-06-02 21:19:26 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	let product = {
+		id: '',
+		name: '',
+		description: '',
+		category: '',
+		imageURL: '',
+	}
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		console.log(event)
+	}
+
+</script>
+
+<main>
+	<form on:submit={handleSubmit}>
+		<input 
+			type="text" 
+			placeholder="Product name" 
+			bind:value={product.name}
+		/>
+		<textarea 
+			bind:value={product.description}
+			id="product-description" 
+			rows="3" 
+			placeholder="product Description..."
+		/>
+
+		<input 
+		type="url" 
+		id="product-image-url" 
+		placeholder="https:yourimage.com"
+		bind:value={product.imageURL}
+		/>
+		
+		<select bind:value={product.category} id="category">
+			<option value="laptops">laptops</option>
+			<option value="servers">servers</option>
+			<option value="PC">PC</option>
+		</select>
+		<button>save product!</button>
+	</form>
+</main>
+
+<style>
+</style>
